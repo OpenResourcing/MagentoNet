3bd98f221c7959561aa190321a76e70824b9ac6d
Matthew Dilley <matt.dilley@mdoresourcing.com>
2014-06-26 19:11:52 +0100

    adding android app to the mix

---
+++ MagentoNetApp/assets/www/js/index.js
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+		
+	    var url = "http://192.168.1.109:8001/MagentoNetCategory.svc";
+            var request = "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+                    "<s:Body>" +
+                        "<GetTestString xmlns=\"http://tempuri.org/\">" +
+                        "</GetTestString>" +
+                    "</s:Body>" +
+                "</s:Envelope>";
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: request,
+                contentType: "text/xml",
+                datatype: "xml",
+                headers: { "SOAPAction": "http://tempuri.org/ICategoryContract/GetTestString",
+                "Access-Control-Allow-Headers":"X-PINGOTHER"},
+                success: function (data, textStatus, jqXHR) {                    
+                    	alert("the resultString: " + $(data).find("GetTestStringResult").text());
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert("error: "+jqXHR.responseText+" : "+errorThrown + " : " + textStatus );
+                }
+            });
+		
+        console.log('Received Event: ' + id);
+    }
+};
