112d8cfc9ab152d253518222f6a9299ed418f9a3
Matt Dilley <matt.dilley@mdoresourcing.com>
2014-07-01 13:58:56 +1000

    more advances on the backbone/razor views (namely the CatItem View)

---
+++ MagentoNetMvc/Content/js/catitem-init.js
@@ -0,0 +1,32 @@
+﻿TestModel = Backbone.Model.extend({
+	defaults:	{
+		nameValue : "defaultName",
+		ageValue : "defaultAge",
+		descriptionValue : "defaultDescription"
+	}
+});
+
+ListView = Backbone.View.extend({
+ 	events: {
+    	"click #search_button" : "select",
+    	"click #age" : "select2"
+  	},
+  	initialize: function() {
+	  	this.model.on('change', this.render, this);
+  	},
+	render : function () {
+		var template = _.template( $("#search_template").html(), this.model.toJSON() );
+		this.$el.html(template);
+		this.$el.css("background-color","blue");
+		this.$(".redclass").css("background-color","red");
+		return this;
+	},
+	select : function (){
+		this.model.set({"ageValue" : "40"});
+	},
+	select2 : function (){
+		this.model.set({"nameValue" : "changed!"});
+	}
+
+});
+
