f82156fdf0c2285613d611a8953c82b79f4878a7
Matt Dilley <matt.dilley@mdoresourcing.com>
2014-07-02 23:24:24 +1000

    trying out a winform possibly an suitable alternative to GTK# ?
    completely a RND task for now

---
+++ winforms/SimpleWFApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace winforms
+{
+	class Program
+	{
+		static void Main()
+		{
+			Application.Run (new MainWindow ());
+		}
+	}
+
+	class MainWindow : Form {}
+}
+
