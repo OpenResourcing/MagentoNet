2daab187905fcbe5f8b1eb465edf7bcd5f70984d
Matt Dilley <matt.dilley@mdoresourcing.com>
2014-07-08 21:38:25 +1000

    while .net-based wcfservice is not yet ready, we are making use of Magento-php's own SOAP service

--- MagentoNetMvc/Controllers/CatItemController.cs
+++ MagentoNetMvc/Controllers/CatItemController.cs
@@ -5,25 +5,43 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Ajax;
 using MagentoNetMvc.MagentoNetCategoryClient;
+using MagentoNetMvc.mg_dev.mdoresourcing.com;
 using System.ServiceModel;
+using System.Configuration;
 
 //using tempuri.org;
 namespace MagentoNetMvc.Controllers
 {
 	public class CatItemController : Controller
 	{
+		static String sessionId = null;
 		public ActionResult Index (int id)
 		{
 			ViewBag.Message = "Welcome to ASP.NET MVC on Mono! (viewbag)";
 			//			ViewData ["Message"] = "Welcome to ASP.NET MVC on Mono!";
-
+/*
+			// keeping this endpoint reference here in preps for when this service is ready for use
+			// ... until then we will use the SOAP services provided by the Magento-php
 			var binding = new BasicHttpBinding ();
 			var address = new EndpointAddress( "http://192.168.1.109:8001/MagentoNetCategory.svc");
 
 			ICategoryContract client = new CategoryContractClient (binding, address);
 			GetCatItemResult catItem = client.GetCatItem(id);
+*/
 
-			ViewBag.catItem = catItem;
+
+			var soapConnectionString = System.Configuration.ConfigurationManager.AppSettings ["MySOAPConnectionString"];
+			MagentoService client = new MagentoService(soapConnectionString);
+			if (String.IsNullOrEmpty(sessionId)){
+				var soapUsername = System.Configuration.ConfigurationManager.AppSettings ["MySOAPUserName"];
+				var soapPassword = System.Configuration.ConfigurationManager.AppSettings ["MySOAPPassword"];
+				sessionId = client.login (soapUsername, soapPassword);
+			}
+			catalogCategoryInfo catInfo = client.catalogCategoryInfo(sessionId, id, "1", null);
+			ViewBag.sessionId = sessionId;
+			ViewBag.catInfo = catInfo;
+
+
 			return View ();
 		}
 
